dda32feb7aa018c454364064146c4985124f6312
Kotaro Sugawara <kotarella1110@gmail.com>
2022-07-28 19:28:17 +0900

    refactor: improve lifycycles export

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,15 +4,11 @@ import singleSpaReact from "single-spa-react";
 
 export const App = (): JSX.Element => <h1>React App is mounted!</h1>;
 
-export const lifecycles = singleSpaReact({
+export const { bootstrap, mount, unmount } = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: App,
   domElementGetter: () => document.getElementById("root")!,
 });
-
-export const bootstrap = lifecycles.bootstrap;
-export const mount = lifecycles.mount;
-export const unmount = lifecycles.unmount;
 
 export default App;
