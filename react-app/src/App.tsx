3fd93afc7625d7de21f516b4338d87168682d642
Kotaro Sugawara <kotarella1110@gmail.com>
2022-07-29 20:46:39 +0900

    chore: fix typo

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import singleSpaReact from "single-spa-react";
-import { About } from "./components/Abount";
-import { Home } from "./components/Home";
 import { NavBar } from "./components/NavBar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
 import { Topics } from "./components/Topics";
 
 export const App = () => (
